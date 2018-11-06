e5d89d5a90c663ff0e2710902d9d9f85607cb485
Awet Fessha <awetkifle94@gmail.com>
2018-11-06 23:43:36 +0000

    add license

--- product-plugin.php
+++ product-plugin.php
@@ -6,14 +6,12 @@ Description: Display products and add products to table
 Version: 0.1.0
 Author: Awet Fessha
 Author URI: https://github.com/AKFessha/product-plugin
+License: GPL version 2 or any later version
 Text Domain: product-plugin
 Domain Path: /languages
 */
 
 //your code goes here
-//Testing add action
-// add_action('wp_head', function(){
-//     echo '<script> alert("Welcome to my site!")</script>';
-// });
+
 
 
